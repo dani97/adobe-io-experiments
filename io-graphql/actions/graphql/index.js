489a377744823a1262356aa292ff29a5817eb197
cdaniel <cdaniel@adobe.com>
2023-11-09 15:13:48 +0530

    review service

--- io-graphql/actions/graphql/index.js
+++ io-graphql/actions/graphql/index.js
@@ -1,7 +1,6 @@
 /*
 * <license header>
 */
-
 /**
  * This is a sample action showcasing how to access an external API
  *
@@ -13,24 +12,19 @@
  *   - Make sure to validate these changes against your security requirements before deploying the action
  */
 
-
 const fetch = require('node-fetch')
 const { Core } = require('@adobe/aio-sdk')
 const { graphql, buildSchema } = require('graphql');
 const { errorResponse, stringParameters, checkMissingRequestInputs } = require('../utils');
-
 // main function that will be executed by Adobe I/O Runtime
 async function main (params) {
   // create a Logger
   const logger = Core.Logger('main', { level: params.LOG_LEVEL || 'info' })
-
   try {
     // 'info' is the default level if not set
     logger.info('Calling the main action')
-
     // log parameters, only if params.LOG_LEVEL === 'debug'
     logger.debug(stringParameters(params));
-
     const requiredParams = ['query'];
     const requiredHeaders = [];
     const errorMessage = checkMissingRequestInputs(params, requiredParams, requiredHeaders);
@@ -38,39 +32,86 @@ async function main (params) {
       // return and log client errors
       return errorResponse(400, errorMessage, logger);
     }
-
     var schema = buildSchema(`
-      type Query {
-        bestTweets: [Tweets]
-      }
-
-      type Tweets {
-        text: String
-        handle: String
-        sku: String
-      }
+    type Query {
+      productReviews(sku: String!): [Reviews]
+      productsReviews(sku: [String!]!): [productReviewslist]
+    }
+    
+    type Reviews {
+      text: String
+      handle: String
+      sku: String
+    }
+    
+    type Review {
+      text: String
+      handle: String
+    }
+    
+    type productReviewslist {
+      sku : String
+      reviews: [Review]
+    }
+    
+      
     `);
+    const reviews = [
+      {
+        'text' : "Very good product",
+        'handle': 'ajaz',
+        'sku' : 'VT12'
+      },
+      {
+        'text' : "Doesn't disappoint",
+        'handle': 'simran',
+        'sku' : 'VD03'
+      },
+      {
+        'text' : "worst product ever",
+        'handle': 'chris',
+        'sku' : 'VD03'
+      }
+    ]
+    
 
     var resolver = {
-      bestTweets: () => {
-        return [{
-          'text' : "Very good product",
-          'handle': 'hi123',
-          'sku' : 'WSH07'
-        },
-        {
-          'text' : "Doesn't disappoint",
-          'handle': 'play123',
-          'sku' : '24-MB05'
-        }
-      ]
-    }};
-
+          productReviews : ( {sku}  ) => {
+            const filteredReviews =   reviews.filter((review) => review.sku === sku);
+            console.log(filteredReviews);
+            const productReviews = filteredReviews.map((review) => ({
+            text: review.text,
+            handle: review.handle,
+            sku: review.sku,
+      }));
+      return productReviews;
+      }
+      ,
+        productsReviews :( {sku}  ) => {
+        const result = [];
+        reviews.forEach((review) => {
+          if (sku.includes(review.sku)) {
+            let productReviews = result.find(
+              (item) => item.sku === review.sku
+            );
+            if (!productReviews) {
+              productReviews = { sku: review.sku, reviews: [] };
+              result.push(productReviews);
+            }
+            productReviews.reviews.push({
+              text: review.text,
+              handle: review.handle,
+            });
+          }
+  });
+  return result;
+  
+      },
+    };
+    
     var context = {};
 
-
     const variables = typeof (params.variables) === 'string' ? JSON.parse(params.variables) : params.variables;
-
     var response = await graphql({
       schema: schema,
       source: params.query,
@@ -79,12 +120,10 @@ async function main (params) {
       variableValues: variables,
       operationName: params.operationName
     });
-
     return {
       statusCode: 200,
       body: response
     };
-
     // check for missing request input parameters and headers
   } catch(e) {
     logger.error(e);
@@ -94,5 +133,4 @@ async function main (params) {
     };
   }
 }
-
 exports.main = main;
