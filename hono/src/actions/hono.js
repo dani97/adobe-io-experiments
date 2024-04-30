c3ae45bd22e013c313be91b7d28bb95fee1104e5
Christopher Daniel <cdaniel@adobe.com>
2024-04-30 15:00:54 +0530

    added response handling

--- hono/src/actions/hono.js
+++ hono/src/actions/hono.js
@@ -8,28 +8,47 @@ async function main (params) {
   // create a Logger
   const logger = Core.Logger('main', { level: params.LOG_LEVEL || 'info' })
   
+  // routes declaration
   app.get('/hello', (c) => {
     return c.json({message: "hello app builder"})
   })
 
-  app.all('/test', (c) => {
+  app.get('/test', (c) => {
     return c.json({ test: "this is a different route", query: c.req.query()})
   })
 
+  // default route
+  app.all('/', (c) => {
+    return c.json({ message: "Welcome to homepage"});
+  })
+
+  // error handler
+  app.onError((err, c) => {
+    console.error(`${err}`)
+    return c.text('Server Error', 500)
+  })
+
+  // create headers for request
   const options = {
     headers: params['__ow_headers'],
     method: params['__ow_method'],
   }
+  
+  // create body for request
   if (params['__ow_body']) {
     options.body = params['__ow_body']
   }
 
+  // capture URL search params
   const query = new URLSearchParams(getQueryParams(params));
 
+  // call hono server and return response
   const response = await app.request(params['__ow_path'] +'?' + query.toString(), options);
+
   return {
-    statusCode: 200,
-    body: await response.json()
+    statusCode: response.status,
+    body: await response.json(),
+    headers: response.headers
   } 
 }
 
